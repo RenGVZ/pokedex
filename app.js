132de23e74e4b59b605f7539405cb34d10672c5b
gavingarza <gaving@live.com>
2020-04-11 19:23:15 +0900

    refactoring code to reduce fetch redundancy

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 const pokedex = document.getElementById('pokedex');
-console.log(pokedex);
 
 const fetchPokemon = async () => {
   const url = `https://pokeapi.co/api/v2/pokemon?/limit=150`;
   const res = await fetch(url);
   const data = await res.json();
-  const pokemon = data.results.map((result, index)
-  => ({
-      name: data.name,
-      id = index + 1,
-
-  })
-  )
+  const pokemon = data.results.map((result, index) => 
+  ({
+      ...result,
+      id: index + 1,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`,
+  }));
+  displayPokemon(pokemon);
 };
 
 const displayPokemon = (pokemon) => {
-  const pokemonHtmlString = pokemon.map(pokeman => `
-  <li class="card">
+  console.log(pokemon);
+  const pokemonHtmlString = pokemon.map((pokeman) => `
+  <li class="card" onclick="selectPokemon(${pokeman.id}">
     <img class="card-image" src="${pokeman.image}" />
     <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-    <p class="card-subtitle">Type: ${pokeman.type}</p>
   </li>
   `
     )
     .join('');
   pokedex.innerHTML = pokemonHtmlString;
+}
+
+const selectPokemon = async (id) => {
+ console.log(id);
 }
 fetchPokemon();
